ddd5b91162b4e02f91f53f193296d9418fbbd76b
Ghunter2001 <stephenganzo.2001@gmail.com>
2024-04-22 16:08:35 +0800

    "Changes made"

--- serverLogin.js
+++ serverLogin.js
@@ -13,28 +13,15 @@ const pool = createPool({
 
 const app = express();
 
-// // Middleware for parsing request bodies
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Middleware for managing sessions
-// app.use(session({
-//     secret: 'your-secret-key',
-//     resave: false,
-//     saveUninitialized: true
-// }));
-
-
 
 app.get("/login.html", (req, res) => {
-
-    // req.session.destroy();
-
     res.sendFile(__dirname + "/login.html");
 });
 
 
 // Logout endpoint
 app.get("/logout", (req, res) => {
+    // req.session.destroy();
     res.redirect("/login.html");
 });
 
@@ -49,16 +36,10 @@ app.post("/login", (req, res) => {
             return;
         }
 
-        // Query to check username and password in the "enrolledStudents" table
         const userQuery = `SELECT * FROM enrolledstudents WHERE student_number = '${username}' AND password = '${pass}'`;
-
-        // Query to check username and password in the "admin" table
         const adminQuery = `SELECT * FROM admin WHERE username = '${username}' AND pass = '${pass}'`;
-
-        // Query to check username and password in the "users" table
         const teacherQuery = `SELECT * FROM faculty WHERE username = '${username}' AND pass = '${pass}'`;
 
-        // Execute all queries in parallel
         Promise.all([
             new Promise((resolve, reject) => {
                 connection.query(userQuery, (err, userResult) => {
@@ -124,47 +105,6 @@ app.post("/signup", (req, res) => {
         }
     });
 });
-
-
-
-
-// //Signup Endpoint
-// app.post("/signup", (req, res) => {
-//     // Retrieve user data from the request body
-//     const { fname, lname, course, year, section, email, password } = req.body;
-
-//     // Create a connection pool and connect to the database
-//     const connection = new sql.ConnectionPool(config);
-//     connection.connect(function (err) {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).send("Server error");
-//         } else {
-//             const request = new sql.Request(connection);
-//             const query = `INSERT INTO dbo.users (fname, lname, course, year, section, email, password) 
-//                        VALUES ('${fname}', '${lname}', '${course}', '${year}', '${section}', '${email}', '${password}')`;
-
-//             request.query(query, (err, result) => {
-//                 if (err) {
-//                     console.log(err);
-//                     res.status(500).send("Server error");
-//                 } else {
-//                     console.log("Signup successful");
-//                     res.send(`
-//               <script>
-//                 alert("Signup successful!");
-//                 window.location.href = "/login.html"; // Redirect to the login page after successful sign-up
-//               </script>
-//             `);
-//                 }
-
-//                 connection.close(); // Close the connection
-//             });
-//         }
-//     });
-// });
-
-
 
 
 module.exports = app;
