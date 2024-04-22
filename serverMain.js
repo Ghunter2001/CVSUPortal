2b19c9bdf99591bed5d324b50657726c3b3143d8
Ghunter2001 <stephenganzo.2001@gmail.com>
2024-04-22 23:35:05 +0800

    serverMain

--- serverMain.js
+++ serverMain.js
@@ -5,17 +5,6 @@ const admin = require("./serverAdmin");
 const teacher = require("./serverTeacher");
 
 
-const { createPool } = require('mysql');
-
-const pool = createPool({
-    host: process.env.MYSQL_ADDON_HOST,
-    user: process.env.MYSQL_ADDON_USER,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB
-});
-
-module.exports = pool;
-
 
 const app = express();
 app.use("/css", express.static("css"));
