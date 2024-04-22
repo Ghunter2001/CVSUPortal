245353351b21be3e363f3b346b979fba79665821
Ghunter2001 <stephenganzo.2001@gmail.com>
2024-04-22 18:17:54 +0800

    "login"

--- serverLogin.js
+++ serverLogin.js
@@ -1,7 +1,6 @@
 const express = require("express");
 // const bodyParser = require('body-parser');
 // const session = require('express-session');
-
 require('dotenv').config();
 const { createPool } = require("mysql");
 
