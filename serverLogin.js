081c61bccfdb12d57815d1e09d8136fb13fbf357
Ghunter2001 <stephenganzo.2001@gmail.com>
2024-04-22 17:04:14 +0800

    dotenv

--- serverLogin.js
+++ serverLogin.js
@@ -1,15 +1,19 @@
 const express = require("express");
 // const bodyParser = require('body-parser');
 // const session = require('express-session');
+
+require('dotenv').config();
 const { createPool } = require("mysql");
 
 
+
 const pool = createPool({
-    host: "boqsxje636m1zh7arjwz-mysql.services.clever-cloud.com",
-    user: "uqmghk5cebbhshej",
-    password: "YbrMP14IoaJzXTZBbRKv",
-    database: "boqsxje636m1zh7arjwz",
-})
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT
+  });
 
 const app = express();
 
