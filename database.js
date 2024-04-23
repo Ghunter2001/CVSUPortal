7b93d13b3875806c583b9fe3886fd4613d866ea8
Ghunter2001 <stephenganzo.2001@gmail.com>
2024-04-24 03:00:19 +0800

    database

--- database.js
+++ database.js
@@ -2,18 +2,11 @@ const { createPool } = require('mysql');
 
 
 const pool = createPool({
-    host: process.env.MYSQL_ADDON_HOST,
-    user: process.env.MYSQL_ADDON_USER,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB,
-    port: process.env.MYSQL_ADDON_PORT
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT
 });
-
-// const pool = createPool({
-//     host: process.env.MYSQL_ADDON_HOST,
-//     user: process.env.MYSQL_ADDON_USER,
-//     password: process.env.MYSQL_ADDON_PASSWORD,
-//     database: process.env.MYSQL_ADDON_DB
-// });
 
 module.exports = pool;
